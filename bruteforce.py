4338efe3d08260a19779a23c0a5d0f7ed761237d
Myriam Denizot <58182901+WolfSilver0509@users.noreply.github.com>
2023-01-09 11:25:49 +0100

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,62 +1,60 @@
-
 import csv
 import json
 
 from itertools import combinations
 
-
 with open('forcebrute.csv') as data:
     data = [d for d in csv.DictReader(data, delimiter=';')]
-    
 
 combinaisons = []
 for i in range(1, 20):
-  comb = combinations(data, i)
-  combinaisons.extend(list(comb))
+    comb = combinations(data, i)
+    combinaisons.extend(list(comb))
 
-
-print(len(combinaisons))
-
+# print(len(combinaisons))
 
 
 def sumcomb(comb):
-  total_price = 0
-  for element in comb:
-    total_price += int(element["price"])
-  return total_price
+    total_price = 0
+    for element in comb:
+        total_price += int(element["price"])
+    return total_price
+
 
 new_combinaisons = []
 
 for comb in combinaisons:
-  comb_price = sumcomb(comb)
-  if comb_price <= 500:
-    new_combinaisons.append(comb)
+    comb_price = sumcomb(comb)
+    if comb_price <= 500:
+        new_combinaisons.append(comb)
 
 
 def calculer_profit(comb):
-  total_profit =0
-  for element in comb:
-    total_profit += (int(element['profit']) * int(element['price'])) / 100
-  return total_profit
+    total_profit = 0
+    for element in comb:
+        total_profit += (int(element['profit']) * int(element['price'])) / 100
+    return total_profit
+
 
 best_combination = new_combinaisons[0]
 best_total_profit = calculer_profit(best_combination)
 
 for comb in new_combinaisons:
-  total_profit= calculer_profit(comb)
-  if total_profit > best_total_profit:
-    best_total_profit = total_profit
-    best_combination = comb
+    total_profit = calculer_profit(comb)
+    if total_profit > best_total_profit:
+        best_total_profit = total_profit
+        best_combination = comb
 
-    
-print(best_total_profit)
-print(best_combination)
-total_price = sumcomb(best_combination)
-print(total_price)
+# print(best_total_profit)
+print(f'Le meilleur profit est de {best_total_profit}€ pour la combinaison suivante :')
+for element in best_combination:
+    print(element['name'])
+# print(best_combination)
+print(f'Le prix total de cette combinaison est de {sumcomb(best_combination)}€')
+# total_price = sumcomb(best_combination)
+# print(total_price)
 
-  
-
-# comparer le temps d'exécution et remettre au propre les prints avec des phrses plus juste nom des actions 
+# comparer le temps d'exécution et remettre au propre les prints avec des phrases plus juste nom des actions
 
 
 
